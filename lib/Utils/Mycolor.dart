d452fe88f7ae7078236889c09209bf902e1a7247
neel6 <poojapatel3535@gmail.com>
2024-07-03 06:29:20 -0600

    - started work utils

---
+++ lib/Utils/Mycolor.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+class MyColor {
+  static const Color backgroundLogin = Color(0xFFF3EBF6);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color purple = Color(0xFF8C55AA);
+  static const Color deepPurple = Color(0xFF673AB7);
+  static const Color loginBtnColor = Color(0xFF28a745);
+  static const Color darkCyan = Color(0xFF008B8B);
+  static const Color cardIconColor = Color(0xFFdddfeb);
+  static const Color cardIColorRed = Color(0xFFe74a3b);
+  static const Color cardIColorBlue = Color(0xFF202C59);
+  static const Color cardIColorIndigo = Color(0xFF6f42c1);
+  static const Color cardIColorYellow = Color(0xFFf6c23e);
+  static const Color lightPink = Color(0xFFF7CED7);
+  static const Color pastelBlue = Color(0xFFAED9E0);
+  static const Color naturalGreen = Color(0xFF65A34B);
+  static const Color softBlue = Color(0xFF91C9E8);
+  static const Color earthyStone = Color(0xFFC1A06B);
+  static const Color brightBlue = Color(0xFF00A8E8);
+  static const Color yellowGold = Color(0xFFFFD700);
+}
---
+++ lib/Utils/globalText.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class GlobalText extends StatelessWidget {
+  final String text;
+  final String? fontFamily;
+  final Color? color;
+  final double? fontSize;
+  final double? letterSpacing;
+  final TextOverflow? textOverflow;
+  final FontWeight? fontWeight;
+  final TextDecoration? textDecoration;
+  final FontStyle? fontStyle;
+  final TextAlign? textAlign;
+  final double? height;
+  final int? maxLine;
+  final VoidCallback? onTap;
+  final bool? isTextSelectable;
+  final List<Shadow>? shadows;
+  final double? paddingTop, paddingBottom, paddingRight, paddingLeft;
+  final TextStyle? textStyle;
+  const GlobalText(
+    this.text, {
+    Key? key,
+    this.height,
+    this.fontSize,
+    this.fontWeight,
+    this.fontFamily,
+    this.color,
+    this.textDecoration,
+    this.textOverflow,
+    this.fontStyle = FontStyle.normal,
+    this.letterSpacing,
+    this.textAlign,
+    this.maxLine,
+    this.onTap,
+    this.isTextSelectable = false,
+    this.shadows,
+    this.paddingTop,
+    this.paddingBottom,
+    this.paddingRight,
+    this.paddingLeft,
+    this.textStyle,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final fontTextStyle = TextStyle(
+      fontFamily: 'Ubuntu' ?? ' sans-serif ',
+      color: color,
+      height: height ?? 1.2,
+      overflow: textOverflow,
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      decoration: textDecoration,
+      fontStyle: fontStyle,
+      letterSpacing: letterSpacing,
+      shadows: shadows,
+    );
+    return isTextSelectable == true
+        ? SelectableText(
+            text,
+            textAlign: textAlign,
+            maxLines: maxLine,
+            style: textStyle ?? fontTextStyle,
+          )
+        : Text(
+            text,
+            textAlign: textAlign,
+            maxLines: maxLine,
+            style: textStyle ?? fontTextStyle,
+          );
+  }
+}
